74e62c13cf830881bb6e5a3c155f0778fefd4d98
esternielsen <esterannang@gmail.com>
2025-12-30 19:58:10 +0100

    feat: add possibility go back and login from register page along with some styling

--- frontend/src/app/shared/auth/register/register.component.ts
+++ frontend/src/app/shared/auth/register/register.component.ts
@@ -4,7 +4,7 @@ import { MatButton } from '@angular/material/button';
 import { MatCard, MatCardHeader, MatCardTitle, MatCardSubtitle, MatCardContent } from '@angular/material/card';
 import { MatFormField, MatLabel, MatError } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '@auth/auth.service';
 import { passwordMatchValidator } from '../../validators/password-match.validator';
 
@@ -23,6 +23,7 @@ import { passwordMatchValidator } from '../../validators/password-match.validato
     MatCardHeader,
     MatCardTitle,
     MatCardSubtitle,
+    RouterLink,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
