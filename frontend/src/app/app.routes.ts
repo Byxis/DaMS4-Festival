20d446637f360ac982388ef4fcab1a18f96ad649
AndreaAntoniali <andreaoreste.antoniali@gmail.com>
2026-01-02 17:25:11 +0100

    "feat and refactor": new popup form to add a festival, and clean up of the code.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { AdminComponent } from '@admin/admin/admin.component';
 import { authGuard } from '@auth/auth.guard';
 import { adminGuard } from '@admin/admin.guard';
 import { FestivalList } from './festivals/festival-list-component/festival-list';
+import { FestivalsPage } from './pages/festivals-page/festivals-page';
 
 export const routes: Routes = [
   { path: 'login', component: LoginPage },
 
   { path: 'home', component: HomePage, canActivate: [authGuard] },
-  { path: 'festivals', component: FestivalList, canActivate: [authGuard] },
+  { path: 'festivals', component: FestivalsPage, canActivate: [authGuard] },
   { path: 'admin', component: AdminComponent, canActivate: [authGuard, adminGuard] },
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: '**', redirectTo: 'home' },
--- frontend/src/app/festivals/festival-list-component/festival-list.ts
+++ frontend/src/app/festivals/festival-list-component/festival-list.ts
@@ -2,10 +2,8 @@ import { Component, signal, WritableSignal, effect, computed, inject } from '@an
 import { FestivalCard } from '../festival-card-component/festival-card';
 import { FestivalService } from '../festival-service/festival-service';
 import { MatIcon } from '@angular/material/icon';
-import { MatDialog } from '@angular/material/dialog';
 import { FestivalNewFormComponent } from "../festival-new-form-component/festival-new-form-component";
 import { MatButtonModule } from "@angular/material/button";
-import { MatCard } from "@angular/material/card";
 
 @Component({
   selector: 'app-festival-list',
@@ -14,34 +12,9 @@ import { MatCard } from "@angular/material/card";
   styleUrl: './festival-list.scss'
 })
   export class FestivalList {
-    private readonly dialog = inject(MatDialog);
 
     constructor() {
       effect(() => { this.svc.loadFestivalsFromServer() });
     }
-
     readonly svc = inject(FestivalService);
-
-
-    //Change the state of showForm to show form
-    openFormDialog(): void {
-      const dialogRef = this.dialog.open(FestivalNewFormComponent, {
-        width: '600px',
-        disableClose: false
-      });
-
-      dialogRef.afterClosed().subscribe(result => {
-        if (result) {
-          this.onFestivalCreated();
-        }
-      });
-    }
-
-    onFestivalCreated() {
-      this.svc.loadFestivalsFromServer(); // Actualise la liste
-    }
-
-
-    removeFestival(id: number): void { this.svc.removeFestival(id) }
-
   }
