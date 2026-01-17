10b764e530d074102962aded2e8a322c54d1f673
alexis.serrano <alexis.serrano@etu.umontpellier.fr>
2026-01-17 22:04:22 +0100

    Refactor: redesigned festival card layout and styles
    Refactored the festival card component to improve its visual layout and structure. Added a new banner section for festival name, dates, and location with icon, updated background image handling, and enhanced overall styling for better appearance and consistency. Also included MatIconModule in the component imports.

--- frontend/src/app/festivals/festival-card-component/festival-card.ts
+++ frontend/src/app/festivals/festival-card-component/festival-card.ts
@@ -1,39 +1,40 @@
-import { DatePipe } from '@angular/common';
-import { Component, input, inject } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { FestivalDto } from '../dtos/festival-dto';
-import { Router } from '@angular/router';
+import {DatePipe} from '@angular/common';
+import {Component, inject, input} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {Router} from '@angular/router';
+
+import {FestivalDto} from '../dtos/festival-dto';
 
 @Component({
-  selector: 'app-festival-card',
-  standalone: true,
-  imports: [MatCardModule, MatButtonModule, DatePipe],
-  templateUrl: './festival-card.html',
-  styleUrl: './festival-card.scss'
+    selector: 'app-festival-card',
+    standalone: true,
+    imports: [MatCardModule, MatButtonModule, DatePipe, MatIconModule],
+    templateUrl: './festival-card.html',
+    styleUrl: './festival-card.scss'
 })
-export class FestivalCard {
-  festival = input.required<FestivalDto>();
-  isHovered = false;
+export class FestivalCard
+{
+    festival = input.required<FestivalDto>();
+    isHovered = false;
 
-  private readonly router = inject(Router)
+    private readonly router = inject(Router)
 
 
-  navigateToFestivalPage(): void {
-    console.log("You clicked on ", this.festival().id)
-    if (this.festival().id) {
-      this.router.navigate(["/festivals", this.festival().id]);
+    navigateToFestivalPage(): void
+    {
+        console.log("You clicked on ", this.festival().id)
+        if (this.festival().id)
+        {
+            this.router.navigate(["/festivals", this.festival().id]);
+        }
     }
-  }
 
 
-    getInitials(): string {
-    const name = this.festival().name;
-    return name
-      .split(' ')
-      .map(word => word[0])
-      .join('')
-      .toUpperCase()
-      .slice(0, 2);
-  }
+    getInitials(): string
+    {
+        const name = this.festival().name;
+        return name.split(' ').map(word => word[0]).join('').toUpperCase().slice(0, 2);
+    }
 }
