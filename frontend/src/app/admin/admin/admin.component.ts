a301cd05deb43d89ead6dde62cda886e785e3a46
esternielsen <esterannang@gmail.com>
2026-01-17 16:36:47 +0100

    feat: add user list component to show list of users in the admin page

--- frontend/src/app/admin/admin/admin.component.ts
+++ frontend/src/app/admin/admin/admin.component.ts
@@ -1,57 +1,49 @@
-import { Component, effect, inject, WritableSignal } from '@angular/core';
+import { Component, inject, WritableSignal } from '@angular/core';
 import { UserDto } from '../../shared/types/user-dto';
 import { UserService } from '@users/user.service';
-import { UserCard } from '@users/user-card/user-card';
-import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
-import { MatChip } from '@angular/material/chips';
-import { MatDivider } from '@angular/material/divider';
-import { MatList } from '@angular/material/list';
 import { MatDialog } from '@angular/material/dialog';
 import { CreateUserDialog } from '@admin/create-user-dialog/create-user-dialog.component';
-import { AuthService } from '@auth/auth.service';
+import { UsersListComponent } from "@admin/users-list/users-list.component";
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'admin',
-  imports: [MatCard, MatCardContent, MatIcon, MatChip, MatList, MatDivider],
+  imports: [MatIcon, UsersListComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
 export class AdminComponent {
   private readonly userSvc = inject(UserService);
   private readonly dialog = inject(MatDialog);
+  private readonly snack = inject(MatSnackBar);
   readonly users: WritableSignal<UserDto[]> = this.userSvc.users;
-  // Charge la liste à l’arrivée sur la page
-  constructor() {
-    effect(() => this.userSvc.loadAll());
-    console.log(this.users()[0]);
-  }
 
   displayedColumns: string[] = ['firstName', 'lastName', 'role', 'email'];
 
   openCreateDialog(): void {
-      const dialogRef = this.dialog.open(CreateUserDialog, {
-        data: null,
-        width: '600px',
-      });
-  
-      dialogRef.afterClosed().subscribe((result) => {
-        if (result?.user) {
-          this.userSvc.createUser(result.user);
-        }
-      });
-    }
-
-  openEditDialog(user: UserDto): void {
     const dialogRef = this.dialog.open(CreateUserDialog, {
-        data: user,
-        width: '600px',
-      });
-  
-      dialogRef.afterClosed().subscribe((result) => {
-        if (!result?.user?.id) return;
-        this.userSvc.editUser(result.user as UserDto);
-        
-      });
-}
+      data: null,
+      width: '600px',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result?.user) {
+        this.userSvc.createUser(result.user).subscribe({
+          next: (res) => {
+            this.userSvc.users.set([...this.userSvc.users(), res.user]);
+
+            this.snack.open(res.message, "OK", { duration: 2500 });
+          },
+          error: (err) => {
+            this.snack.open(err?.error?.error ?? "Erreur serveur", "OK", {
+              duration: 3500,
+            });
+          },
+        });
+      }
+    });
+  }
+
 
 }
---
+++ frontend/src/app/admin/users-list/users-list.component.ts
@@ -0,0 +1,63 @@
+import { CreateUserDialog } from '@admin/create-user-dialog/create-user-dialog.component';
+import { Component, effect, inject, WritableSignal } from '@angular/core';
+import { MatCard, MatCardContent } from '@angular/material/card';
+import { MatChip } from '@angular/material/chips';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { MatList } from '@angular/material/list';
+import { UserService } from '@users/user.service';
+import { UserDto } from '../../shared/types/user-dto';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+@Component({
+  selector: 'app-users-list',
+  imports: [MatChip, MatList, MatDivider, MatIcon, MatCard, MatCardContent],
+  templateUrl: './users-list.component.html',
+  styleUrl: './users-list.component.scss'
+})
+export class UsersListComponent {
+  private readonly userSvc = inject(UserService);
+  readonly users: WritableSignal<UserDto[]> = this.userSvc.users;
+  readonly dialog = inject(MatDialog);
+  readonly snack = inject(MatSnackBar);
+  // Charge la liste à l’arrivée sur la page
+  constructor() {
+    effect(() => this.userSvc.loadAll());
+    console.log(this.users()[0]);
+  }
+  displayedColumns: string[] = ['firstName', 'lastName', 'role', 'email'];
+
+  openEditDialog(user: UserDto): void {
+  const dialogRef = this.dialog.open(CreateUserDialog, {
+    data: user,
+    width: '600px',
+  });
+
+  dialogRef.afterClosed().subscribe((result) => {
+    if (!result?.user?.id) return;
+
+    this.userSvc.editUser(result.user as UserDto).subscribe({
+      next: (res) => {
+        const current = this.users();
+        const idx = current.findIndex(u => u.id === res.user.id);
+        if (idx !== -1) {
+          current[idx] = res.user;
+          this.users.set([...current]);
+        }
+
+        // show server message
+        this.snack.open(res.message, "OK", { duration: 2500 });
+      },
+      error: (err) => {
+        this.snack.open(err?.error?.error ?? "Erreur serveur", "OK", {
+          duration: 3500,
+        });
+      }
+    });
+  });
+}
+
+}
+
